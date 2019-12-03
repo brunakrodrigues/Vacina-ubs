18d5e6bd03f0bfb00c8afb2e7f448c653f55ef19
Bruna Rodrigues <40840258+brunakrodrigues@users.noreply.github.com>
2019-12-03 13:28:53 -0300

    Add files via upload

---
+++ Appsettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UBS_mvc
+{
+    public class Appsettings
+    {
+        public string ConnectionString { get; set; }
+    }
+}
